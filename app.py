480308f2088163934a077f1cfbf4b4ec5a43ce3b
ella-walker <wella2@byu.edu>
2025-11-12 13:41:55 -0700

    app.py created

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title = "Hello Streamlit", layout = "wide")
+
+st.title("Hello , Streamlit")
+st.write("This app reruns top - to - bottom whenever a widget changes.")
